a5313082409ffeb3ad96d012f97857d72f008559
Artsiom Shushkevich <artemshushkevich@live.com>
2017-12-11 01:36:08 +0300

    Add some plugins and loaders usage

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('webpack').optimize.UglifyJsPlugin;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new UglifyJsPlugin(),
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ],
   module: {
     rules: [
       {
@@ -18,7 +31,29 @@ module.exports = {
             presets: ['env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                sourceMap: true 
+              } 
+            }
+          ]
+        })
       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/'),
+    compress: true,
+    port: 9000,
+    // stats: 'errors-only',
+    open: true
   }
 }; 
